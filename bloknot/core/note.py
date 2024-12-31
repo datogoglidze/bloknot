d0cf3e421e6711522785a0bc1aea432f0d0e839c
Schwifter <d.goglidze@sangu.edu.ge>
2024-12-31 21:52:56 +0400

    Generate current date

--- bloknot/core/note.py
+++ bloknot/core/note.py
@@ -1,11 +1,12 @@
+import time
 from dataclasses import dataclass, field
 from uuid import uuid4
 
 
 @dataclass(frozen=True)
 class Note:
-    date: str
     content: str
     title: str | None = None
+    date: str = field(default_factory=lambda: str(int(time.time())))
 
     id: str = field(default_factory=lambda: str(uuid4()))
